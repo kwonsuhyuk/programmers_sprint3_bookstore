430a7170c897e9ca2a3313f96ae54c6bdfa8f1d3
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-06-04 17:01:20 +0900

    6/4

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,12 +5,12 @@ import Title from "../components/common/Title";
 function Home() {
   return (
     <>
-      <Title size="large">제목 </Title>
+      {/* <Title size="large">제목 </Title>
       <div>home body</div>
       <Button size="large" scheme="primary">
         버튼 테스트
       </Button>
-      <InputText placeholder="여기에 입력하세요" />
+      <InputText placeholder="여기에 입력하세요" /> */}
     </>
   );
 }
---
+++ src/pages/Order.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+function Order() {
+  const location = useLocation();
+  const orderDataFromCart = location.state;
+
+  return <div>Order</div>;
+}
+
+export default Order;
